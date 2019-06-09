6f642c35c2cf8683cf5eedbcd0fcf14af8bfa105
Kiarash Golzadeh <kiarash.gl@gmail.com>
2019-06-09 13:40:07 +0430

    Read Mnist files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.io.File;
+import java.util.List;
 public class Main {
+    private final static String TRAIN_LABELS_ADDRESS = "C:\\Users\\Kiarash\\Desktop\\AI\\AiProject\\src\\Resource\\Train\\train-labels-idx1-ubyte";
+    private final static String TRAIN_IMAGES_ADDRESS = "C:\\Users\\Kiarash\\Desktop\\AI\\AiProject\\src\\Resource\\Train\\train-images-idx3-ubyte";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] labels = MnistReader.getLabels(TRAIN_LABELS_ADDRESS);
+        List<int[][]> images = MnistReader.getImages(TRAIN_IMAGES_ADDRESS);
     }
 }
