3eacfdf6d013a27e113de115342215ac342de038
Mahsa Sheikhi <mahsa_sh09@yahoo.com>
2019-06-11 19:47:33 +0430

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.io.File;
 import java.util.List;
 public class Main {
-    private final static String TRAIN_LABELS_ADDRESS = "C:\\Users\\Kiarash\\Desktop\\AI\\AiProject\\src\\Resource\\Train\\train-labels-idx1-ubyte";
-    private final static String TRAIN_IMAGES_ADDRESS = "C:\\Users\\Kiarash\\Desktop\\AI\\AiProject\\src\\Resource\\Train\\train-images-idx3-ubyte";
+    private final static String TRAIN_LABELS_ADDRESS = "/Users/mahsa_shv/Desktop/AiProject/src/Resource/Train/train-labels-idx1-ubyte";
+    private final static String TRAIN_IMAGES_ADDRESS = "/Users/mahsa_shv/Desktop/AiProject/src/Resource/Train/train-images-idx3-ubyte";
     public static void main(String[] args) {
         int[] labels = MnistReader.getLabels(TRAIN_LABELS_ADDRESS);
         List<int[][]> images = MnistReader.getImages(TRAIN_IMAGES_ADDRESS);
